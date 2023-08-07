5f9ac080aaea8d4277a6ecb6e7f37250894720a6
marko-londo <londo.py@gmail.com>
2023-08-07 18:09:29 -0400

    edits

--- Dashboard/Pages/page1.py
+++ Dashboard/Pages/page1.py
@@ -2,14 +2,23 @@
 import streamlit as st
 
 def show():
-    st.markdown("")
-    st.markdown("<h1 style='text-align: center;'>Home<br><br></h1>", unsafe_allow_html=True)
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.write(' ')
+
+    with col2:
+        st.image("https://github.com/marko-londo/Suicide-Analysis/blob/main/Resources/Images/candles.png?raw=true", use_column_width="auto")
+
+    with col3:
+        st.write(' ')
+    st.markdown("<br><br>", unsafe_allow_html=True)
     st.write("""Welcome to the interactive Suicide Data Dashboard – a
-             powerful tool designed to shed light on a pressing and often
-             overlooked societal issue. 
-             The mission is to provide you with a comprehensive understanding
-             of suicide data, 
-             enabling you to explore key insights and trends from various angles.
+            powerful tool designed to shed light on a pressing and often
+            overlooked societal issue. 
+            The mission is to provide you with a comprehensive understanding
+            of suicide data, 
+            enabling you to explore key insights and trends from various angles.
 
 In a world increasingly driven by data, it is crucial to confront complex
 topics like suicide with informed analysis. 
@@ -25,7 +34,7 @@ age ranges to identify patterns, outliers, and potential influencing factors.
 
 - Global Comparisons: Gain a global perspective by comparing suicide rates with other countries, highlighting both shared challenges and unique differences.
 - Gender Insights: Examine suicide data through a gender lens, recognizing the
-  distinct experiences and vulnerabilities that different genders face.
+distinct experiences and vulnerabilities that different genders face.
 
 As you navigate through this dashboard, remember that each data point represents a life, a story, and a call to action. By engaging with this tool, you contribute to the ongoing dialogue on mental health, destigmatizing discussions around suicide, and fostering a more compassionate society.
 
